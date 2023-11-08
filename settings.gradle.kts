f2f85bc048a135f1f9f184cb0f3d1c1d178758d9
South <southernscotts112@gmail.com>
2023-11-07 22:46:48 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Blue Ridge Final Project App"
+include(":app")
+ 
